e76434c0570d1cc347e56ee216d244287457502c
Joshua Tompkins <josh@joshtompkins.com>
2014-03-27 21:32:45 -0400

    Updated for style.

--- src/players/ai_player.rb
+++ src/players/ai_player.rb
@@ -7,7 +7,7 @@ class AiPlayer < Player
 	end
 
 	def find_open_corner
-		#select a random open corner
+		# select a random open corner
 		corners = [:upper_left, :upper_right, :lower_left, :lower_right].shuffle!
 
 		loop do
@@ -22,7 +22,7 @@ class AiPlayer < Player
 	end
 
 	def find_open_edge
-		#select a random open edge
+		# select a random open edge
 		edges = [:left, :right, :top, :bottom].shuffle!
 
 		loop do
@@ -41,30 +41,30 @@ class AiPlayer < Player
 	end
 
 	def move
-		#look for a winning move, and make it if it exists
+		# look for a winning move, and make it if it exists
 		winning = find_threatening @marker
 
 		return @board.set winning.first_empty, @marker if winning
 
-		#look for a threatening move, and block it if it exists
+		# look for a threatening move, and block it if it exists
 		threatening = find_threatening identify_other_player
 
 		return @board.set threatening.first_empty, @marker if threatening
 
-		#no winning or threatening moves, so let's just pick a spot
-		#first, check the corners
+		# no winning or threatening moves, so let's just pick a spot
+		# first, check the corners
 		open_corner = find_open_corner
 
 		return @board.set open_corner, @marker if open_corner
 
-		#corner isn't open, check the center
+		# corner isn't open, check the center
 		return @board.set :center, @marker unless @board.set? :center
 
-		#center isn't open, check the edges
+		# center isn't open, check the edges
 		open_edge = find_open_edge
 
 		return @board.set open_edge, @marker if open_edge
 
-		#nothing is open, so just bail
+		# nothing is open, so just bail
 	end
 end
